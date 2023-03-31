c8bc024eeed49c634105a293c295339e659aaeac
GuilhermeNSouza <guilhermenariodesouza@gmail.com>
2023-03-31 13:54:34 -0400

    commit geral

--- js/signin.js
+++ js/signin.js
@@ -2,7 +2,6 @@ function logar(){
   let login = document.getElementById('login').value;
   let senha = document.getElementById('senha').value;
   if(login =="admin"&& senha=="admin"){
-    alert('Sucesso');
     location.href = "home.html";
   } else {
     alert('Usuario ou senha incorretos')
